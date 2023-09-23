7af5f8bfb57f0a753e784bbc468d288671ab7378
Michael Latzoni <mlatzoni@gmail.com>
2023-09-23 13:12:09 -0700

    refactor: tailwind prefix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="tw-flex -tw-min-h-screen tw-flex-col tw-items-center tw-justify-between tw-p-24">
             <h1>Charles Fox Guitars</h1>
         </main>
     );
